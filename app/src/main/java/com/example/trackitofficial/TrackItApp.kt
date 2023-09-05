c7f4afef6be0117de1081c4fc2105eb462f93497
Benjamin Ayesu <ben.ayesu.attah@gmail.com>
2023-09-05 01:48:48 -0700

    Added switch for light/dark toggle

--- app/src/main/java/com/example/trackitofficial/TrackItApp.kt
+++ app/src/main/java/com/example/trackitofficial/TrackItApp.kt
@@ -1,18 +1,20 @@
 package com.example.trackitofficial
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material.icons.outlined.AddCircle
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
@@ -42,7 +44,7 @@ fun TrackItTopAppBar(
 ) {
     CenterAlignedTopAppBar(
         title = { Text(title) },
-        modifier = modifier,
+        modifier = modifier.padding(dimensionResource(id = R.dimen.padding_small)),
         scrollBehavior = scrollBehavior,
         navigationIcon = {
             if (canNavigateBack) {
@@ -52,6 +54,11 @@ fun TrackItTopAppBar(
                         contentDescription = stringResource(R.string.back_button)
                     )
                 }
+            }
+        },
+        actions = {
+            Row {
+                Switch(checked = true, onCheckedChange = {})
             }
         }
     )
