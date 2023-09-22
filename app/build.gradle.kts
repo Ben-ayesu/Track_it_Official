3937833d0e5bea945f074333b1868a55dc7eb359
benjaminayesu-attah <ben.ayesu.attah@gmail.com>
2023-09-21 23:45:57 -0700

    Added passing test

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -76,6 +76,10 @@ dependencies {
     // Testing
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    testImplementation("org.mockito:mockito-core:3.12.4")
+    testImplementation("org.mockito:mockito-junit-jupiter:3.12.4")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.5.1")
+
 
     // Navigation
     implementation("androidx.navigation:navigation-compose:2.6.0")
