a8bb744e63bf9d216dd64f971a9f78ec78c70610
benjaminayesu-attah <ben.ayesu.attah@gmail.com>
2023-09-21 20:44:38 -0700

    Got rid of tool tip

--- app/src/main/java/com/example/trackitofficial/ui/create/CreateWorkoutScreen.kt
+++ app/src/main/java/com/example/trackitofficial/ui/create/CreateWorkoutScreen.kt
@@ -1,35 +1,24 @@
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.defaultMinSize
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Info
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusDirection
 import androidx.compose.ui.platform.LocalFocusManager
-import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
@@ -110,7 +99,9 @@ fun CreateWorkoutBody(
             onClick = onSaveClick,
             enabled = workoutUiState.isEntryValid,
             shape = MaterialTheme.shapes.small,
-            modifier = Modifier.fillMaxWidth()
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(dimensionResource(id = R.dimen.padding_medium))
         ) {
             Text(text = stringResource(R.string.save_action))
         }
@@ -176,29 +167,20 @@ fun CreateWorkoutInputForm(
                 modifier = Modifier.padding(start = dimensionResource(id = R.dimen.padding_small))
             )
         }
-        Row(
-            modifier = Modifier,
-            verticalAlignment = Alignment.CenterVertically,
-        ) {
-            IconButton(onClick = {}) {
-                Icon(imageVector = Icons.Default.Info, contentDescription = "Info")
-            }
-            Text(text = "Tap check in the top right to save workout")
-        }
     }
 }
 
-@Composable
-fun ColorView() {
-    Box(
-        modifier = Modifier
-            .width(5.dp)
-            .fillMaxHeight()
-            .padding(start = 10.dp)
-            .background(colorResource(id = R.color.select_color))
-    )
-}
-@Preview()
+//@Composable
+//fun ColorView() {
+//    Box(
+//        modifier = Modifier
+//            .width(5.dp)
+//            .fillMaxHeight()
+//            .padding(start = 10.dp)
+//            .background(colorResource(id = R.color.select_color))
+//    )
+//}
+@Preview
 @Composable
 private fun CreateWorkoutScreenPreview() {
     TrackItOfficialTheme {
