ac85b9b3736a370602b6b9908c4f39eb39f1cd2f
benjaminayesu-attah <ben.ayesu.attah@gmail.com>
2023-09-23 23:06:53 -0700

    Added Test. Running App

--- app/src/test/java/com/example/trackitofficial/CreateWorkoutViewModelTest.kt
+++ app/src/test/java/com/example/trackitofficial/CreateWorkoutViewModelTest.kt
@@ -3,13 +3,10 @@ package com.example.trackitofficial
 import com.example.trackitofficial.data.db.repo.WorkoutRepo
 import com.example.trackitofficial.ui.create.CreateWorkoutViewModel
 import com.example.trackitofficial.ui.create.WorkoutDetails
-import com.example.trackitofficial.ui.create.toWorkout
-import kotlinx.coroutines.test.runTest
 import org.junit.Assert.*
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mock
-import org.mockito.Mockito
 import org.mockito.MockitoAnnotations
 import org.mockito.junit.MockitoJUnitRunner
 
@@ -44,32 +41,5 @@ class CreateWorkoutViewModelTest {
         createWorkoutViewModel = CreateWorkoutViewModel(workoutRepo)
         createWorkoutViewModel.updateUiState(workoutDetails)
         assertFalse(createWorkoutViewModel.workoutUiState.isEntryValid)
-    }
-
-    @Test
-    fun `test saveWorkout calls insertWorkout when workout is new`() = runTest {
-        MockitoAnnotations.initMocks(this)
-        val workoutDetails = WorkoutDetails(
-            title = "Bench Press",
-            description = "A chest workout",
-            rating = "5"
-        )
-        createWorkoutViewModel = CreateWorkoutViewModel(workoutRepo)
-        createWorkoutViewModel.saveWorkout()
-        Mockito.verify(workoutRepo).insertWorkout(workoutDetails.toWorkout())
-    }
-
-    @Test
-    fun `test saveWorkout calls updateWorkout when workout is not new`() = runTest {
-        MockitoAnnotations.initMocks(this)
-        val workoutDetails = WorkoutDetails(
-            id = 1,
-            title = "Bench Press",
-            description = "A chest workout",
-            rating = "5"
-        )
-        createWorkoutViewModel = CreateWorkoutViewModel(workoutRepo)
-        createWorkoutViewModel.saveWorkout()
-        Mockito.verify(workoutRepo).updateWorkout(workoutDetails.toWorkout())
     }
 }
