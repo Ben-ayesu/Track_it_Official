5054c87a2fca235603a7ff9df74a6c8557d00d92
benjaminayesu-attah <ben.ayesu.attah@gmail.com>
2023-09-29 00:04:26 -0700

    Added Tint

--- app/src/main/java/com/example/trackitofficial/TrackItApp.kt
+++ app/src/main/java/com/example/trackitofficial/TrackItApp.kt
@@ -4,9 +4,11 @@ import android.annotation.SuppressLint
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarScrollBehavior
@@ -55,6 +57,13 @@ fun TrackItTopAppBar(
                         contentDescription = stringResource(R.string.back_button)
                     )
                 }
+            } else {
+                IconButton(onClick = navigateUp) {
+                    Icon(
+                        imageVector = Icons.Filled.Menu,
+                        contentDescription = stringResource(R.string.menu)
+                    )
+                }
             }
         },
         actions = {
@@ -63,7 +72,8 @@ fun TrackItTopAppBar(
             ) {
                 Icon(
                     imageVector = Icons.Filled.DateRange,
-                    contentDescription = stringResource(R.string.date)
+                    contentDescription = stringResource(R.string.date),
+                    tint = MaterialTheme.colorScheme.onSurface
                 )
             }
         }
